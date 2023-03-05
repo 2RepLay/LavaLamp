d99f8a83e9e689a8b08e394eae1acf106aed66c5
Nikita Yankov <evne.yankovnikita@gmail.com>
2023-03-05 12:24:45 +0200

    Initial Commit

---
+++ LavaLamp/LavaLampApp.swift
@@ -0,0 +1,17 @@
+//
+//  LavaLampApp.swift
+//  LavaLamp
+//
+//  Created by nikita on 05.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct LavaLampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
